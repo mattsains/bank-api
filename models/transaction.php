6a46c978b12aaca44b1aecf03fcc6bab6b8a1039
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-09-03 18:25:24 +0200

    issue #15: transaction model is vastly improved. Now returns either end-user info, or admin-type info.

--- models/transaction.php
+++ models/transaction.php
@@ -7,29 +7,45 @@ class Transaction extends CI_Model {
 		parent::__construct();
     }
 	/// Returns a list of transactions involving an account since the date specified.
-	/// if no time is specified, returns all transactions.
-	/// format: {{'tid':transaction id, 'timestamp':timestamp of transaction, 'fromid':aid the transaction comes from, 'toid':aid transaction goes to.},,}
-	function get_since($aid, $timestamp=0)
+	/// if no time is specified, returns all transactions
+	/// if alltext is true, this function will return data raw from the database, and will not generate new text if neccessary
+	/// format: who knows
+	function get_since($aid, $timestamp=0,$alltext=false)
 	{
 		$timestamp=(int)$timestamp;
 		$aid=(int)$aid;
-		
-		$this->db->where('fromid',$aid);
-		$this->db->or_where('toid',$aid);
-		
-		if ($timestamp!==0)
-			$this->db->where('timestamp >=',$timestamp);
+		$alltext=(bool)$alltext;
+		$this->db->select('tid,timestamp,amount,frombankid,fromid,fromtext,tobankid,toid,totext');//everything except the log text
+		$where='((fromid='.$aid.' AND frombankid=0) OR (toid='.$aid.' AND tobankid=0)) AND timestamp>='.$timestamp;
+		$this->db->where($where);
 		$return=array();
-		foreach($this->db->get('transact')->result() as $row)
-		{
-			$return[]=array('tid'=>(int)$row->tid, 
-			                'timestamp'=>(int)$row->timestamp, 
-							'amount'=>(float)$row->amount, 
-							'fromid'=>(int)$row->fromid, 
-							'toid'=>(int)$row->toid,
-							'text'=>$row->fromid===$aid?$row->fromtext:$row->totext);
+		if ($alltext)
+	    {
+			foreach($this->db->get('transact')->result() as $row)
+				$return[]=array('tid'=>(int)$row->tid, 'timestamp'=>(int)$row->timestamp, 'amount'=>(float)$row->amount, 'frombankid'=>(int)$row->frombankid, 'fromid'=>(int)$row->fromid, 'fromtext'=>$row->fromtext, 'tobankid'=>(int)$row->tobankid, 'toid'=>(int)$row->toid, 'totext'=>$row->totext);
+			return $return;
 		}
-		return $return;
+		else
+		{
+			foreach ($this->db->get('transact')->result() as $row)
+			{
+				$returnrow=array('tid'=>(int)$row->tid, 'timestamp'=>(int)$row->timestamp);
+				if ($row->fromid==$aid && $row->frombankid==0)
+				{
+					$returnrow['amount']=-$row->amount;
+					if ($row->fromtext=='') $returnrow['text']=$this->get_text($row->tid,true);
+					else $returnrow['text']=$row->fromtext;
+				}
+				elseif ($row->toid==$aid && $row->tobankid==0)
+				{
+					$returnrow['amount']=$row->amount;
+					if ($row->totext=='') $returnrow['text']=$this->get_text($row->tid,false);
+					else $returnrow['text']=$row->totext;
+				}
+				$return[]=$returnrow;
+			}
+			return $return;
+		}
 	}
 	/// debits an account and credits another with an amount specified.
 	/// VERY INSECURE AT THE MOMENT - anyone can transfer anything
@@ -47,5 +63,40 @@ class Transaction extends CI_Model {
 					'fromtext'=>$fromtext);
 		$this->db->insert('transact',$data);
 		return $this->db->insert_id();
+	}
+	///gets bigtext log of transaction
+	function get_log($tid)
+	{
+		$tid=(int)$tid;
+		
+		$this->db->select('log');
+		$this->db->where('tid',$tid);
+		$result=$this->db->get('transact');
+		if ($result->num_rows()<1) return false;
+		return $result->row()->log;
+	}
+	/// figures out the transaction text
+	/// $isfrom: true->return fromtext; false->return totext
+	function get_text($tid, $isfrom)
+	{
+	$isfrom=(bool)$isfrom;
+	$tid=(int)$tid;
+	
+	if ($isfrom) $this->db->select('toid,fromid,fromtext AS text');
+	else $this->db->select('totext AS text');
+	$this->db->where('tid',$tid);
+	$result=$this->db->get('transact');
+	if ($result->num_rows<1) return false;//transaction doesn't exist
+	$row=$result->row();
+	if ($row->text!='') return $row->text;//text already exists
+	
+	//if we get here, we need to make text and cache it in the database.
+	if ($isfrom)
+		$text='bank transfer to '.$row->toid;
+	else $text='bank transfer from '.$row->fromid;
+	
+	$this->db->where('tid',$tid);
+	$this->db->update('transact',array($isfrom?'fromtext':'totext'=>$text));
+	return $text;
 	}
 }
