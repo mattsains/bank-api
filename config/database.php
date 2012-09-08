f25c89f5bec05e1ea0bfffc94d307d15dad5c334
Matthew Sainsbury <matthew@sainsbury.za.net>
2012-09-08 11:26:05 +0200

    config changes (oops)

--- config/database.php
+++ config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'arthurdent';
+$db['default']['username'] = 'bankapi';
+$db['default']['password'] = 'sb7dfyGyTZaXCT3H';
 $db['default']['database'] = 'bank';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
